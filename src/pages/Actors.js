b8a156ebb47430b23c207558b5fe32d088613a8f
Paps <Mikealfredmuigai@gmail.com>
2024-05-11 11:38:11 +0300

    intial commit

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
+import Card from "../components/ReusableCard";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+  let[actors, setActors] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:3000/actors")
+    .then(r => r.json())
+    .then(data => setActors(data))
+    .catch(error => console.error(error))
+  }, [])
+
+  let actorList = actors.map(actor => <Card key={actor.id} name={actor.name} movies={actor.movies} />)
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar/>
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actorList}
       </main>
     </>
   );
